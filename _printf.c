a354ead8cb74b44e0b0ff5e098774bfc48dd2819
ESTHER EJIMOFOR <ejimofor81@gmail.com>
2023-03-28 22:03:24 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer [], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
+ * @print a series of characters and values to the standard output
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
