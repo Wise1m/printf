2d425c36bde045bace67097b97fcba0f428bdeab
ESTHER EJIMOFOR <ejimofor81@gmail.com>
2023-03-28 23:01:21 +0100

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -4,6 +4,7 @@
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
+ * @width: print color width 
  * @list: list of arguments.
  *
  * Return: width.
