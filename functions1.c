91a045339289fc9066fc3aea5917df35cbc86984
ESTHER EJIMOFOR <ejimofor81@gmail.com>
2023-03-28 22:17:16 +0100

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -9,6 +9,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
+ * pointer to a char
  * Return: Number of chars printed.
  */
 int print_unsigned(va_list types, char buffer[],
